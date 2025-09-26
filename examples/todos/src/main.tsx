8fb77f00391235181bfb2bccd73f2c08b655a50b
Jorge Jacome <jorge2002_mx@hotmail.com>
2025-09-25 22:00:00 -0400

    example(todos): add index.lyra.tsx route

--- examples/todos/src/main.tsx
+++ examples/todos/src/main.tsx
@@ -3,9 +3,5 @@ import App from "./app/routes/index.lyra.tsx";
 import { mount } from "@lyra-dev/runtime";
 
 const root = document.getElementById("app")!;
-
-// render the page component
 render(<App />, root);
-
-// activate Lyra directive bindings (on:*, class:*, data-*)
 mount(root);
